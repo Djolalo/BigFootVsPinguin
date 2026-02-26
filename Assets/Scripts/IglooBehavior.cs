185362e6b5e0a717a810f951aec273c551f095c8
Noa Coinon <noa.coinon.etu@univ-lille.fr>
2026-02-26 15:39:59 +0100

    Controller and Pinguin spawn and interact

--- Assets/Scripts/IglooBehavior.cs
+++ Assets/Scripts/IglooBehavior.cs
@@ -1,16 +1,99 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class IglooBehavior : MonoBehaviour
 {
+
+    public GameObject pinguin_warriors;
+
+    private List<GameObject> pinguins = new List<GameObject>();
+    [SerializeField]
+    private GameObject pingu_sentry; 
+
     // Start is called before the first frame update
+    [SerializeField]
+    private int nb_vague = 0;
+
+    private int nb_pinguin = 8;
+
+    private bool SendWave = false;
+
+    [SerializeField]
+    private int nbSentinels;
+
+    private bool WaveAlreadyTriggered = false;
+
+    void Awake()
+    {
+        if(pinguins == null) pinguins = new List<GameObject>();
+    }
     void Start()
     {
-        
+        //creating pingus at the start
+        for(int i = 0 ; i<nbSentinels; i++){
+            SpawnSentry();
+        } 
     }
 
     // Update is called once per frame
     void Update()
+    { 
+        if(pinguins == null) return;
+
+        // Uni tyEngine.Debug.Log(pinguins.Count);
+        if (SendWave && pinguins.Count == 0)
+        {
+            int i = 0;
+            nb_pinguin = (nb_vague == 0 )? nb_pinguin: nb_pinguin << 1;
+            while( i++ < nb_pinguin)
+            {
+                SpawnWarrior();
+            }
+            nb_vague ++;
+            if(nb_vague > 3)
+            {
+                SendWave = false;
+                nb_vague = 0;
+            }
+        }
+    }
+
+    private void SpawnSentry()
     {
-        
+        GameObject pingu = (GameObject)Instantiate(pingu_sentry, this.transform.position, this.transform.rotation);
+        PinguinBehavior pb = pingu.GetComponent<PinguinBehavior>();
+        pb.init(this.transform, this);
+        pinguins.Add(pingu);
+
+    }
+
+    private void SpawnWarrior()
+    {
+        GameObject pingu = (GameObject)Instantiate(pinguin_warriors, this.transform.position, this.transform.rotation);
+        PinguinWarriorBehavior pb = pingu.GetComponent<PinguinWarriorBehavior>();
+        pb.init(this.transform, this);
+        pinguins.Add(pingu);
+
+    }
+
+    public void PinguinDied(GameObject pinguin)
+    {
+        if(pinguins.Contains(pinguin)) pinguins.Remove(pinguin);
+        Destroy(pinguin);
+    }
+
+    public void Wave()
+    {
+        if (!WaveAlreadyTriggered)
+        {
+            WaveAlreadyTriggered = true;
+            foreach(GameObject pingu in pinguins)
+            {
+                Destroy(pingu);
+            }
+            pinguins.Clear();
+            SendWave = true;
+        }
+
     }
 }
--- Assets/Scripts/PinguinBehavior.cs
+++ Assets/Scripts/PinguinBehavior.cs
@@ -1,18 +1,77 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class PinguinBehavior : MonoBehaviour
-{
+{  
+    private UnityEngine.AI.NavMeshAgent m_agent;
+    [SerializeField]
+    private float attack_distance;
+    
+    private Transform iglooT;
+    private Transform targetT;
+
+    private IglooBehavior iglooBehavior; 
+
+
+    private float last_chrono;
+    [SerializeField]
+    private float range_of_stranding;
+    [SerializeField]
+    private float range_of_detection;
+    [SerializeField]
+    private float bipolarTime;
+
+    private bool triggeredDetection = false;
     // Start is called before the first frame update
     void Start()
     {
-        
+        m_agent = GetComponent<UnityEngine.AI.NavMeshAgent>();
+        last_chrono = Time.time;
+        this.targetT = GameObject.FindWithTag("Player").transform;
+        updateTarget();
     }
+
+    public void init(Transform iglooT,  IglooBehavior iglooBehavior)
+    {
+        this.iglooT = iglooT; 
+        this.iglooBehavior = iglooBehavior;
+    }
+
 
     // Update is called once per frame
     void Update()
     {
+        if (!triggeredDetection)
+        {
+            bool yeti_Detected = Vector3.Distance(m_agent.transform.position, targetT.position) 
+                            < range_of_stranding;  
+            //Le pingouin est trop loin du yeti et le pingouin n'a pas changé de direction depuis trop longtemps
+            if( !yeti_Detected && Time.time - last_chrono > bipolarTime)
+            {
+                last_chrono = Time.time;
+                updateTarget();
+            } 
+            //rien à faire du chrono, c'est la guerre
+            if(yeti_Detected){
+                m_agent.isStopped = false;
+                m_agent.destination = iglooT.position;
+                triggeredDetection = true;
+
+            }
+        }
+        else
+        {
+            if(Vector3.Distance(iglooT.position, transform.position) < 2)
+            {
+                iglooBehavior.Wave();
+            }
+        }
         
+    }
+
+    private void updateTarget()
+    {
+        float random_x = Random.Range(-1f,1f)*Mathf.PI;
+        Vector3 new_targ_pos = new Vector3(iglooT.position.x + range_of_stranding * Mathf.Cos(random_x),iglooT.position.y,iglooT.position.z + range_of_stranding * Mathf.Sin(random_x));
+        m_agent.destination = new_targ_pos;
     }
 }
